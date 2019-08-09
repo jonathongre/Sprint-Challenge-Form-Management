9a3add1dc013d59aad595f24decebff8a6e4ab18
Jonathon Green <ericg9811@gmail.com>
2019-08-09 13:25:54 -0500

    updated styling

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div className="App gradDynamic">
+      <div className="App">
         <h1>Sign In</h1>
         <FormikForm />
       </div>
